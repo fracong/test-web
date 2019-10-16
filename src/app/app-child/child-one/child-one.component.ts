cbae64e58a2cadcf2dedee468a1e9acc4dee2f37
fracong <237093889@qq.com>
2019-10-16 09:55:05 +0800

    子路由的添加

--- src/app/app-child/child-one/child-one.component.ts
+++ src/app/app-child/child-one/child-one.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare let $: any;
 
 @Component({
   selector: 'app-child-one',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child-one.component.scss']
 })
 export class ChildOneComponent implements OnInit {
+  ifVisible = false;
+  ifOneFocusCLass = false;
+  oneValueList = ['aaaaa', 'bbbbb', 'ccccc', 'ddddd', 'eeeee', 'fffff'];
 
   constructor() { }
 
   ngOnInit() {
+    this.inputOneValue(this.oneValueList[0]);
   }
 
+  changeIfVisible() {
+    this.ifOneFocusCLass = true;
+    if (this.ifVisible) {
+      this.ifVisible = false;
+    } else {
+      this.ifVisible = true;
+    }
+  }
+
+  changeFocusFalse() {
+    this.ifOneFocusCLass = false;
+    setTimeout(() => {
+      this.ifVisible = false;
+    }, 200);
+  }
+
+  changeFocusTrue() {
+    this.ifOneFocusCLass = true;
+    setTimeout(() => {
+      this.ifVisible = true;
+    }, 200);
+  }
+
+  inputOneValue(value: string) {
+    $('.query-group input.item-shop-floor').val(value);
+  }
 }
